58a3bb7c91fd55898d9ce62094f923fc3d7b0396
levi <zhenweilevi@gmail.com>
2024-12-10 07:33:54 +0800

    Annotation dev

--- Annotation/src/main/java/com/kuang/annotation/Test03.java
+++ Annotation/src/main/java/com/kuang/annotation/Test03.java
@@ -2,9 +2,21 @@ package com.kuang.annotation;
 
 public class Test03 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         A a = new A();
         System.out.println(A.m);
+
+
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        System.out.println(classLoader);
+        ClassLoader classLoader1 = classLoader.getParent();
+        System.out.println(classLoader1);
+        ClassLoader classLoader2 = classLoader1.getParent();
+        System.out.println(classLoader2);
+//        ClassLoader classLoader3 = Class.forName("com.kuang,annotation.Test03").getClassLoader();
+//        System.out.println(classLoader3);
+//双亲委派机制
+        System.out.println(System.getProperty("java.class.path"));
     }
 
 
